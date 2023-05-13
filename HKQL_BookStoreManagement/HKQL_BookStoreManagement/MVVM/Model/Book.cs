985f8840322e8f0f670356171c335ac3c880a844
NguyenQuang179 <79895324+NguyenQuang179@users.noreply.github.com>
2023-05-14 00:16:12 +0700

    Init Book & Top5Book

---
+++ HKQL_BookStoreManagement/HKQL_BookStoreManagement/MVVM/Model/Book.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HKQL_BookStoreManagement.MVVM.ViewModel
+{
+    public class Book
+    {
+        public string _id { get; set; }
+        public string name { get; set; }
+        public string author { get; set; }
+        public string publishedYear { get; set; }
+        public string imagePath { get; set; }
+        public int price { get; set; }
+        public int stock { get; set; }
+        public string category { get; set; }
+        public string __v { get; set; }
+
+    }
+}
+
+// namespace BookDataBinding
+// {
+//     public class Book : INotifyPropertyChanged, ICloneable
+//     {
+//         public String Name { get; set; }
+//         public String Cover { get; set; }
+//         public String Author { get; set; }
+//         public String PublishedYear { get; set; }
+
+//         public event PropertyChangedEventHandler? PropertyChanged;
+
+//         public object Clone()
+//         {
+//             return MemberwiseClone();
+//         }
+//     }
+// }
---
+++ HKQL_BookStoreManagement/HKQL_BookStoreManagement/MVVM/Model/Top5Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HKQL_BookStoreManagement.MVVM.ViewModel
+{
+    class Top5Book
+    {
+        public string id { get; set; }
+        public string title { get; set; }
+        public string author { get; set; }
+        public long stock { get; set; }
+    }
+}
