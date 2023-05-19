890a4fea56b0774189235f23154f7be151b9da11
NguyenQuang179 <79895324+NguyenQuang179@users.noreply.github.com>
2023-05-19 19:48:39 +0700

    Update Dashboard

--- HKQL_BookStoreManagement/HKQL_BookStoreManagement/MVVM/Model/ConnectionString.cs
+++ HKQL_BookStoreManagement/HKQL_BookStoreManagement/MVVM/Model/ConnectionString.cs
@@ -8,8 +8,6 @@ namespace HKQL_BookStoreManagement.MVVM.Model
 {
     public class ConnectionString
     {
-        //public const string connectionString = "https://06c4-115-73-219-158.ngrok-free.app";
         public const string connectionString = "https://wp-project1-quanlybanhang-restfulapi.onrender.com";
-
     }
 }
